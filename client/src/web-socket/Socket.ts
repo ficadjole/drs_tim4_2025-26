476521c30a25e30f4294a893eaeb05c3e37de0db
Filip Velemir <velemirfilip@gmail.com>
2026-02-12 00:38:03 +0100

    Update Socket.ts

--- client/src/web-socket/Socket.ts
+++ client/src/web-socket/Socket.ts
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
-
-export const socket = io("http://localhost:5001", {
+export const socket = io("https://user-service.onrender.com", {
   transports: ["websocket"],
 });
 
