c52db0e51e7a99e9b008581c61b004f3b1c3eee4
Filip Velemir <velemirfilip@gmail.com>
2026-02-12 00:47:00 +0100

    socketio setup for render

--- client/src/web-socket/Socket.ts
+++ client/src/web-socket/Socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
-export const socket = io("https://user-service.onrender.com", {
+export const socket = io(import.meta.env.VITE_GATEWAY_URL, {
+  path: "/socket.io",
   transports: ["websocket"],
 });
 
